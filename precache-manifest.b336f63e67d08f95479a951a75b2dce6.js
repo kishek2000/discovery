97e208d76df84e8d1382440168f71bf8b36a0659
Adi <kishek12@gmail.com>
2020-05-16 16:41:38 +1000

    Updates

---
+++ precache-manifest.b336f63e67d08f95479a951a75b2dce6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bb90dbd4738ca30682ad21694de992",
+    "url": "/discovery/index.html"
+  },
+  {
+    "revision": "75b063d6b663cfc2469d",
+    "url": "/discovery/static/css/main.dec0436e.chunk.css"
+  },
+  {
+    "revision": "4b248654af8bb6141132",
+    "url": "/discovery/static/js/2.844b46f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/discovery/static/js/2.844b46f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b063d6b663cfc2469d",
+    "url": "/discovery/static/js/main.3c860fd8.chunk.js"
+  },
+  {
+    "revision": "44128ae5be8000aab4c4",
+    "url": "/discovery/static/js/runtime-main.50d87a60.js"
+  },
+  {
+    "revision": "a8d5259cb07ba8c55fdc769653788559",
+    "url": "/discovery/static/media/body.a8d5259c.jpg"
+  },
+  {
+    "revision": "5474344b8edf3ab1deeaa2974c9d8652",
+    "url": "/discovery/static/media/bodyIsland.5474344b.png"
+  },
+  {
+    "revision": "80fcb8127ed06eff1f2831199a51d453",
+    "url": "/discovery/static/media/creative.80fcb812.jpeg"
+  },
+  {
+    "revision": "dd96f1c8ba7b7093b45cd891dd3b3ce6",
+    "url": "/discovery/static/media/creativeIsland.dd96f1c8.png"
+  },
+  {
+    "revision": "9bb5c8eef5ae85a04c0ca6fdf3dcdba7",
+    "url": "/discovery/static/media/earthIsland.9bb5c8ee.png"
+  },
+  {
+    "revision": "0cadce299b10a86100cb3931b94d9520",
+    "url": "/discovery/static/media/language.0cadce29.jpg"
+  },
+  {
+    "revision": "a955d27ca1c04139156a36badb7c06b8",
+    "url": "/discovery/static/media/languageIsland.a955d27c.png"
+  },
+  {
+    "revision": "20e42c7f4cbf575ec0131716e3b3c291",
+    "url": "/discovery/static/media/mainBG.20e42c7f.png"
+  },
+  {
+    "revision": "7076c640b3d130906232b6c4dc02e0f5",
+    "url": "/discovery/static/media/space.7076c640.png"
+  },
+  {
+    "revision": "72a30f5206cff142f11176e4b9c801af",
+    "url": "/discovery/static/media/spaceIsland.72a30f52.png"
+  },
+  {
+    "revision": "71135219f86fa75dd4d58261230d0202",
+    "url": "/discovery/static/media/stories.71135219.mp3"
+  },
+  {
+    "revision": "f53f22e4d37e455f209b0f54c35bd5cf",
+    "url": "/discovery/static/media/stories.f53f22e4.jpg"
+  },
+  {
+    "revision": "8190c5844ef06f476dd37772e268eec7",
+    "url": "/discovery/static/media/storyIsland.8190c584.png"
+  }
+]);
